ac1e56c9949513050ceef4cc771635cb1be45f76
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-03-31 02:49:07 +0530

    updated

--- E_Binary_Deque.cpp
+++ E_Binary_Deque.cpp
@@ -13,9 +13,51 @@
 #define rep(i, a, b) for (int i = a; i <= b; i++)
 using namespace std;
 
+int longestSubarrayWithSum(vector<int> &nums, int s)
+{
+    int n = nums.size();
+    int left = 0, right = 0, sum = 0, maxLength = -1;
+
+    while (right < n)
+    {
+        sum += nums[right];
+
+        while (sum > s && left <= right)
+        {
+            sum -= nums[left];
+            left++;
+        }
+
+        if (sum == s)
+        {
+            maxLength = max(maxLength, right - left + 1);
+        }
+        right++;
+    }
+
+    return maxLength;
+}
 void helper()
 {
-    
+    int n, s;
+    cin >> n >> s;
+    vi nums(n);
+    input(nums);
+
+    int tot = accumulate(nums.begin(), nums.end(), 0);
+    if (tot < s)
+    {
+        cout << -1 << endl;
+    }
+    else if (tot == s)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        int len = longestSubarrayWithSum(nums, s);
+        cout << n - len << endl;
+    }
 }
 
 signed main()
